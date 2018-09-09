e3b2f315bd9377ca6ca62e90e1709dbdc6a2bf95
Johan Murillo <johan_smc@hotmail.com>
2018-09-08 19:10:26 -0500

    add dummy test case

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class DummyTest(TestCase):
+
+    #   This is a dummy test to test Travis
+    def test_travis(self):
+        self.assertEqual('Hello', 'Hello')
